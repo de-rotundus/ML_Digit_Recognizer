f95cd1ee1d532688ad58efe66886f74674eb7ab2
de_rotundus <ganchenko.ru@gmail.com>
2024-02-05 13:18:01 +0300

    Use this like a benchmark

--- CNN_tune_sub.py
+++ CNN_tune_sub.py
@@ -10,6 +10,7 @@ Convolution neural network for digit recognition on mnist dataset
 import torch
 
 from tqdm import tqdm
+from time import time
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -19,10 +20,14 @@ from matplotlib import pyplot as plt
 import torch.nn.functional as F
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset, random_split
+
+from torchvision import disable_beta_transforms_warning
+disable_beta_transforms_warning()
 from torchvision.transforms import v2
 
 from sklearn.metrics import confusion_matrix
 from matplotlib.colors import LogNorm
+from argparse import ArgumentParser
 
 def compute_accuracy(predictions, y):
     """Computes the accuracy of predictions against the gold labels, y."""
@@ -150,107 +155,119 @@ def run_epoch(data, model, optimizer, device=torch.device('cpu'), batch_size=32)
     avg_accuracy = np.mean(batch_accuracies)
     return avg_loss, avg_accuracy
 
-#import data
-all_data = pd.read_csv("train.csv")
-y=all_data.label.to_numpy(dtype='int64')
+def main(args):
+    device=torch.device(args.device)
+    n_epochs=args.epochs
+    batch_size=args.batch_size
 
-img_pix=28
+    #import data
+    all_data = pd.read_csv("train.csv")
+    y=all_data.label.to_numpy(dtype='int64')
+    img_pix=28
+
+    X=all_data.drop(['label'], axis=1, inplace=False).to_numpy(dtype='float')
+    # Scaling data to [0, 1]
+    X=X/np.max(X)
+
+    # Set transform to augment data for training 
+    transf = v2.RandomAffine(degrees=20, scale=(0.9, 1.1))
+    X_set = ImageDataset(X, y, transform=transf)
+
+    tmp_images = [X_set[i][0] for i in range(9)]
+    tmp_labels = [X_set[i][1].numpy() for i in range(9)]
+#    plot_digits(tmp_images, n=3, labels_act=tmp_labels, rnd=False) uncomment for debuging
+
+    X_train_set, X_dev_set=random_split(X_set, [0.9, 0.1], generator=torch.Generator().manual_seed(42))
 
 
-X=all_data.drop(['label'], axis=1, inplace=False).to_numpy(dtype='float')
-# Scaling data to [0, 1]
-X=X/np.max(X)
+    #%% Train model
 
-# Set transform to augment data for training 
-transf = v2.RandomAffine(degrees=20, scale=(0.9, 1.1))
-X_set = ImageDataset(X, y, transform=transf)
+    #################################
+    ## Model specification
+    model = nn.Sequential(
+	      nn.Conv2d(1, 32, (3, 3)),
+	      nn.ReLU(),
+	      nn.MaxPool2d((2, 2)),
+	      nn.Conv2d(32, 64, (3, 3)),
+	      nn.ReLU(),
+	      nn.MaxPool2d((2, 2)),
+	      Flatten(),
+	      nn.Linear(64*5*5,128),
+	      nn.Dropout(),
+	      nn.Linear(128, 10),
+	    )
+    ##################################
 
-tmp_images = [X_set[i][0] for i in range(9)]
-tmp_labels = [X_set[i][1].numpy() for i in range(9)]
-plot_digits(tmp_images, n=3, labels_act=tmp_labels, rnd=False)
+#    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+#    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    print('Program will run on: ', device)
+    model = model.to(device)
 
-X_train_set, X_dev_set=random_split(X_set, [0.9, 0.1], generator=torch.Generator().manual_seed(42))
+#    batch_size = 128
+#    n_epochs = 40
+    start_time=time()
+    train_acc, dev_acc=train_model(X_train_set, X_dev_set, 
+				   model, device=device, 
+				   nesterov=True, 
+				   n_epochs=n_epochs, batch_size=batch_size)
+    print(f"Model took {(time() - start_time):.2f} seconds to do {n_epochs} epochs with batch size {args.batch_size} on {args.device}.")
+    #%% Plot training and validation accuracies in epochs #uncomment for debuging
+#    plt.subplot()
+#    plt.plot(np.arange(1, n_epochs+1), train_acc, 'b', label='Train accuracy')
+#    plt.plot(np.arange(1, n_epochs+1), dev_acc, 'r', label='Validation accuracy')
+#    plt.legend()
+#    plt.xlabel('Epochs')
+#    plt.ylabel('Accuracy')
+#    plt.show()
 
+    #%% Analysis of prediction results on dev data
+    indxs=np.arange(0,len(X))
+    random.shuffle(indxs)
+    out=model(torch.tensor(X[indxs[:10000]].reshape(-1,1,img_pix,img_pix), dtype=torch.float).to(device))
 
-#%% Train model
+    predictions = torch.argmax(out.cpu(), dim=1).numpy()
+    confusion_mtx = confusion_matrix(predictions, y[indxs[:10000]])
 
-#################################
-## Model specification
-model = nn.Sequential(
-          nn.Conv2d(1, 32, (3, 3)),
-          nn.ReLU(),
-          nn.MaxPool2d((2, 2)),
-          nn.Conv2d(32, 64, (3, 3)),
-          nn.ReLU(),
-          nn.MaxPool2d((2, 2)),
-          Flatten(),
-          nn.Linear(64*5*5,128),
-          nn.Dropout(),
-          nn.Linear(128, 10),
-        )
-##################################
+    f,ax = plt.subplots(figsize=(8, 8))
+    sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,  fmt= '1', ax=ax, norm=LogNorm(), cbar=False)
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.title("Confusion Matrix")
+    plt.show()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
-print('Program will run on: ', device)
-model = model.to(device)
+    #%% Evaluate the model on test data and save submission.cvs
 
-batch_size = 128
-n_epochs = 40
-train_acc, dev_acc=train_model(X_train_set, X_dev_set, 
-                               model, device=device, 
-                               nesterov=True, 
-                               n_epochs=n_epochs, batch_size=batch_size)
+#    test_data = pd.read_csv("test.csv")
+#    X_test=test_data.to_numpy(dtype='float')
 
-#%% Plot training and validation accuracies in epochs
-plt.subplot()
-plt.plot(np.arange(1, n_epochs+1), train_acc, 'b', label='Train accuracy')
-plt.plot(np.arange(1, n_epochs+1), dev_acc, 'r', label='Validation accuracy')
-plt.legend()
-plt.xlabel('Epochs')
-plt.ylabel('Accuracy')
-plt.show()
+    #Train model on the validation dataset
+#    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, nesterov=True)
 
-#%% Analysis of prediction results on dev data
-indxs=np.arange(0,len(X))
-random.shuffle(indxs)
-out=model(torch.tensor(X[indxs[:10000]].reshape(-1,1,img_pix,img_pix), dtype=torch.float).to(device))
+#    for epoch in range(1, n_epochs+1):
+#        print("-------------\nEpoch {}:\n".format(epoch))
+#        loss, acc = run_epoch(X_dev_set, model.train(), optimizer, device, batch_size)
+#        print('Train loss: {:.6f} | Train accuracy: {:.6f}'.format(loss, acc))
 
-predictions = torch.argmax(out.cpu(), dim=1).numpy()
-confusion_mtx = confusion_matrix(predictions, y[indxs[:10000]])
+    # Scaling test data to [0, 1]
+#    X_test=X_test/np.max(X_test)
+#    X_test=X_test.reshape(-1, 1, img_pix, img_pix)
 
-f,ax = plt.subplots(figsize=(8, 8))
-sns.heatmap(confusion_mtx, annot=True, linewidths=0.01,  fmt= '1', ax=ax, norm=LogNorm(), cbar=False)
-plt.xlabel("Predicted Label")
-plt.ylabel("True Label")
-plt.title("Confusion Matrix")
-plt.show()
+#    model_eval=model.eval()
 
-#%% Evaluate the model on test data and save submission.cvs
+#    out=model_eval(torch.tensor(X_test, dtype=torch.float).to(device))
+	
+#    predict = torch.argmax(out.cpu(), dim=1).numpy()
 
-test_data = pd.read_csv("test.csv")
-X_test=test_data.to_numpy(dtype='float')
+#    predictions = [int(x) for x in predict]
 
-#Train model on the validation dataset
-optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, nesterov=True)
+#    output = pd.DataFrame({'ImageId': range(1, len(X_test)+1), 'Label': predictions})
+#    output.to_csv('submission_digits_CNN4.csv', index=False)
+#    print("Your submission was successfully saved!")
 
-for epoch in range(1, n_epochs+1):
-    print("-------------\nEpoch {}:\n".format(epoch))
-    loss, acc = run_epoch(X_dev_set, model.train(), optimizer, device, batch_size)
-    print('Train loss: {:.6f} | Train accuracy: {:.6f}'.format(loss, acc))
-
-# Scaling test data to [0, 1]
-X_test=X_test/np.max(X_test)
-X_test=X_test.reshape(-1, 1, img_pix, img_pix)
-
-model_eval=model.eval()
-
-out=model_eval(torch.tensor(X_test, dtype=torch.float).to(device))
-    
-predict = torch.argmax(out.cpu(), dim=1).numpy()
-
-predictions = [int(x) for x in predict]
-
-output = pd.DataFrame({'ImageId': range(1, len(X_test)+1), 'Label': predictions})
-output.to_csv('submission_digits_CNN4.csv', index=False)
-print("Your submission was successfully saved!")
+if __name__=='__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--device', type=str, required=True, choices=('cpu', 'cuda', 'mps'), help="Which device to use.")
+    parser.add_argument('--batch_size', type=int, default=32, required=False, help="Training or inference batch size")
+    parser.add_argument('--epochs', type=int, default=40, required=False, help="Number of step to train.")
+    args = parser.parse_args()
+    main(args)
